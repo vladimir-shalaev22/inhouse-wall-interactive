6d9b27d6952a652641c4ccb77c39cdc582cd031a
Вова <vladimir-shalaev22@yandex.ru>
2017-08-31 15:18:35 +0700

    Добавляет интерактивность к страничке

--- src/js/main.js
+++ src/js/main.js
@@ -16,6 +16,14 @@ function circlePointClass(isChecked) {
     'circle__point circle__point--active' : 'circle__point';
 }
 
+function displayItem(isChecked) {
+  return isChecked ? 'inline-block' : 'none';
+}
+
+function displayRow(isChecked) {
+  return isChecked ? 'table-row' : 'none';
+}
+
 function freshCount(oldCount, isChecked) {
   let updatedCount = isChecked ? oldCount - 1 : oldCount + 1;
   return updatedCount < 5 ? updatedCount : 5;
@@ -27,14 +35,27 @@ function freshStatus(oldCount, isChecked) {
 }
 
 $(function initApp() {
+  let listItems = $('.items-list__item').hide();
+  let tableRows = $('.data__item').hide();
   let counterButtonText = $('#button-text');
   let counterTextEl = $('.circle__selected');
   let counterButton = $('.circle__action');
+  let resultBlocks = $('.screen--result, .footer').hide();
+  let interactiveBlock = $('.screen--interactive');
   let selectedCount = 0;
+
+  let targetIndex = [
+    [5, 0], [6, 9], [0, 1], [2, 8], [7, 2], [3, 7], [8, 3], [1, 6], [4, 4], [9, 5]
+  ];
 
   $('.circle__item').each(function initItem(index) {
     let circlePoint = $(this).find('.circle__point');
     let isChecked = false;
+
+    $(window).on('clear-items', function clear() {
+      isChecked = false;
+    });
+
     $(this).click(function (event) {
       let count = selectedCount;
       event.preventDefault();
@@ -44,7 +65,64 @@ $(function initApp() {
       counterTextEl.html(counterText(selectedCount));
       counterButtonText.text(buttonText(selectedCount));
       counterButton.attr('class', buttonClass(selectedCount));
+      listItems.eq(targetIndex[index][0]).css('display', displayItem(isChecked));
+      tableRows.eq(targetIndex[index][1]).css('display', displayRow(isChecked));
+      if (selectedCount === 5 && $(window).width() < 768) {
+        $(window).scrollTop(150);
+      }
     });
+  });
+
+  counterButton.click(function triggerResult(event) {
+    event.preventDefault();
+    if (selectedCount === 5) {
+      history.pushState({state:'result'}, 'Результаты', 'result.html');
+      $('.loading-popup').addClass('loading-popup--active');
+      setTimeout(function showResult() {
+        interactiveBlock.hide();
+        resultBlocks.show();
+        $('.loading-popup').removeClass('loading-popup--active');
+        $(window).scrollTop(0);
+      }, 800);
+    }
+  });
+
+  $('form').each(function initForm() {
+    let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    let form = $(this);
+    let url = form.attr('action');
+    let email = form.find('input[name=email]');
+
+    form.submit(function submitForm() {
+      if (!reg.test(email.val())) {
+        form.addClass('form--error');
+        form.find('.form__label').text('Введите Ваш e-mail!');
+        return false;
+      }
+    });
+
+    email.focus(function removeError() {
+      form.removeClass('form--error');
+      form.find('.form__label').text('Ваш e-mail');
+    });
+  });
+
+  $(window).on('popstate', function updatePage(event) {
+    $('.loading-popup').addClass('loading-popup--active');
+    setTimeout(function showResult() {
+      interactiveBlock.show();
+      resultBlocks.hide();
+      listItems.hide();
+      tableRows.hide();
+      selectedCount = 0;
+      counterTextEl.html(counterText(selectedCount));
+      counterButtonText.text(buttonText(selectedCount));
+      counterButton.attr('class', buttonClass(selectedCount));
+      $(window).trigger('clear-items');
+      $('.circle__point').removeClass('circle__point--active');
+      $('.loading-popup').removeClass('loading-popup--active');
+      $(window).scrollTop(0);
+    }, 500);
   });
 
 });
